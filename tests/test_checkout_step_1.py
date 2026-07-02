d0f8323e7063cd470ff9d74845a62f5322a71d71
Daniel Molina <molina9724@gmail.com>
2026-07-02 07:44:31 -0500

    Add test for successful navigation to checkout step 2 and update imports; create new test file for checkout step 2

--- tests/test_checkout_step_1.py
+++ tests/test_checkout_step_1.py
@@ -1,3 +1,5 @@
+from math import exp
+
 import pytest
 from playwright.sync_api import expect
 
@@ -9,7 +11,7 @@ from sauce_project.data.checkout_step_1_data import (
     LAST_NAME,
     ZIP_CODE,
 )
-from sauce_project.po.pages.base_page import CART_URL
+from sauce_project.po.pages.base_page import CART_URL, CHECKOUT_STEP_2_URL
 from sauce_project.po.pages.cart_page import CartPage
 from sauce_project.po.pages.checkout_step_1_page import CheckoutStepOnePage
 
@@ -49,3 +51,12 @@ def test_04_verify_cancel_button_takes_user_back_to_cart_page(
 ) -> None:
     cart_page: CartPage = checkout_step_1_with_item.get_cart_page()
     expect(cart_page._page).to_have_url(CART_URL)
+
+
+def test_05_verify_user_is_taken_to_checkout_step_2_after_successfully_filling_data_and_pressing_continue_button(
+    checkout_step_1_with_item: CheckoutStepOnePage,
+) -> None:
+    checkout_step_1_with_item.fill_in_checkout_information(
+        first_name=FIRST_NAME, last_name=LAST_NAME, zip_code=ZIP_CODE
+    )
+    expect(checkout_step_1_with_item._page).to_have_url(CHECKOUT_STEP_2_URL)
