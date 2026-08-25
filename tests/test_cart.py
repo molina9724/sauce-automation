2d706f0135a8c1c13187f49485b3277e0360a99d
Daniel Molina Ochoa <molina9724@gmail.com>
2026-08-25 08:48:55 -0500

    Refactor test_cart.py to improve locator usage and enhance test assertions for consistency

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -2,7 +2,8 @@
 import pytest
 from playwright.sync_api import expect
 
-from data.cart_data import ACCESS_CART_PAGE_WITHOUT_LOGIN_ERROR, CART_ITEM_DATA
+from data.cart_data import (ACCESS_CART_PAGE_WITHOUT_LOGIN_ERROR,
+                            ALL_ITEMS_INDEX, CART_ITEM_DATA)
 from po.pages.base_page import CART_URL
 from po.pages.cart_page import CartPage
 from po.pages.checkout_step_1_page import CheckoutStepOnePage
@@ -27,18 +28,19 @@ def test_verify_item_is_removed(cart_page_with_item: CartPage) -> None:
     expect(cart_page_with_item.item).to_have_count(1)
     cart_page_with_item.remove_item(0)
     expect(cart_page_with_item.cart.counter).to_be_hidden()
+    expect(cart_page_with_item.item).to_have_count(0)
 
 
 def test_verify_several_items_can_be_added_to_cart(
     cart_page_with_all_items: CartPage,
 ) -> None:
-    expect(cart_page_with_all_items.item).to_have_count(6)
+    expect(cart_page_with_all_items.item).to_have_count(len(ALL_ITEMS_INDEX))
 
 
 def test_verify_items_remain_in_cart_after_pressing_cancel_in_checkout_step_one_page(
     checkout_step_1_with_item: CheckoutStepOnePage,
 ) -> None:
-    cart_page: CartPage = checkout_step_1_with_item.cart.get_cart_page()
+    cart_page: CartPage = checkout_step_1_with_item.cancel()
     assert cart_page.get_all_products_information() == CART_ITEM_DATA
 
 
