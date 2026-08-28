e8727befbfe7590370dadcca72a42b84bb701086
Daniel Molina <molina9724@gmail.com>
2026-08-28 08:14:15 -0500

    Refactor/checkout step 2 page (#11)
    * Refactor checkout step 2 page to use public access for locators
    
    * Refactor checkout step 2 page to streamline locator usage and remove commented-out code
    
    * Refactor checkout step 2 page tests to use expect assertions and remove unused get_tax method
    
    * Refactor checkout step 2 page to remove unused imports and methods
    
    * Refactor checkout step 2 page to remove unused imports and redefine constants
    
    * Refactor test_checkout_step_2 to rename variable for tax calculations

--- po/pages/checkout_step_2_page.py
+++ po/pages/checkout_step_2_page.py
@@ -1,92 +1,21 @@
-from decimal import Decimal
-from typing import Optional
-
 from playwright.sync_api import Locator, Page
-from playwright.sync_api import TimeoutError as PlaywrightTimeoutError
+
+from po.components.cart_page_item import CartItem
 
 from ..components.cart import Cart
 from ..components.left_menu import Menu
 from ..pages.base_page import BasePage
 
-TAXES: Decimal = Decimal("0.08")
-CURRENCY = "$"
-
-SUBTOTAL: str = "Subtotal"
-TAX: str = "Tax"
-TOTAL: str = "Total"
-
 
 class CheckoutStepTwoPage(BasePage):
     def __init__(self, page: Page, timeout: int = 10000) -> None:
         super().__init__(page, timeout)
-        self._cart_list: Locator = self.locator(".cart_list")
+        self.cart_list: Locator = self.locator(".cart_list")
 
-        self._cart_item: Locator = self.locator(".cart_item")
-        self._item_quantity: Locator = self.locator(".cart_quantity")
-        self._item_name: Locator = self.locator(".inventory_item_name")
-        self._item_description: Locator = self.locator(".inventory_item_desc")
-        self._item_price: Locator = self.locator(".inventory_item_price")
+        self.subtotal: Locator = self.locator(".summary_subtotal_label")
+        self.tax: Locator = self.locator(".summary_tax_label")
+        self.total: Locator = self.locator(".summary_total_label")
 
-        self._subtotal: Locator = self.locator(".summary_subtotal_label")
-        self._tax: Locator = self.locator(".summary_tax_label")
-        self._total: Locator = self.locator(".summary_total_label")
-
+        self.item: CartItem = CartItem(page)
         self.menu: Menu = Menu(self.page)
         self.cart: Cart = Cart(self.page)
-
-    def is_cart_list_displayed(self, timeout: Optional[int] = None) -> bool:
-        timeout_ms: int = self._timeout_ms(timeout)
-        try:
-            self._cart_list.wait_for(state="visible", timeout=timeout_ms)
-            return True
-        except PlaywrightTimeoutError:
-            return False
-
-    def _extract_currency_value(self, locator: Locator, label: str) -> str:
-        value: str = locator.inner_text().strip()
-        currency_index: int = value.find(CURRENCY)
-
-        if currency_index != -1:
-            cleaned_value: str = value[currency_index:]
-            return cleaned_value
-        else:
-            raise RuntimeError(f"{label} doesn't have a currency")
-
-    # def get_subtotal(self, timeout: Optional[int] = None) -> str:
-    #     timeout_ms: int = self._timeout_ms(timeout)
-    #     if self._is_item_displayed(self._subtotal, timeout_ms):
-    #         return self._extract_currency_value(self._subtotal, SUBTOTAL)
-    #     raise RuntimeError(
-    #         f"Timed out waiting for {SUBTOTAL} to be displayed after {timeout_ms} ms"
-    #     )
-
-    def get_subtotal(self, timeout: Optional[int] = None) -> str:
-        timeout_ms: int = self._timeout_ms(timeout)
-        self.get_element(self._subtotal, SUBTOTAL, timeout_ms)
-        return self._extract_currency_value(self._subtotal, SUBTOTAL)
-
-    # def get_element(
-    #     self, locator: Union[Locator, str], label: str, timeout: Optional[int] = None
-    # ) -> Locator:
-    #     timeout_ms: int = self._timeout_ms(timeout)
-    #     if self._is_item_displayed(self.locator(locator), timeout_ms):
-    #         return self.locator(locator)
-    #     raise RuntimeError(
-    #         f"Timed out waiting for {label} to be displayed after {timeout_ms} ms"
-    #     )
-
-    def get_tax(self, timeout: Optional[int] = None) -> str:
-        timeout_ms: int = self._timeout_ms(timeout)
-        if self._is_item_displayed(self._tax, timeout_ms):
-            return self._extract_currency_value(self._tax, TAX)
-        raise RuntimeError(
-            f"Timed out waiting for {TAX} to be displayed after {timeout_ms} ms"
-        )
-
-    def get_total(self, timeout: Optional[int] = None) -> str:
-        timeout_ms: int = self._timeout_ms(timeout)
-        if self._is_item_displayed(self._total, timeout_ms):
-            return self._extract_currency_value(self._total, TOTAL)
-        raise RuntimeError(
-            f"Timed out waiting for {TOTAL} to be displayed after {timeout_ms} ms"
-        )
--- tests/test_checkout_step_2.py
+++ tests/test_checkout_step_2.py
@@ -1,4 +1,6 @@
 # fmt: off
+from playwright.sync_api import expect
+
 from data.cart_data import CART_ITEM_DATA
 from data.checkout_step_2_data import calculate_subtotal, calculate_taxes
 from data.inventory_data import INVENTORY_ITEMS_DATA
@@ -10,28 +12,28 @@ from po.pages.checkout_step_2_page import CheckoutStepTwoPage
 def test_validate_item_subtotal(
     checkout_step_2_page_with_item: CheckoutStepTwoPage,
 ) -> None:
-    assert checkout_step_2_page_with_item.get_subtotal() == calculate_subtotal(
-        CART_ITEM_DATA
-    )
+    expected_subtotal: str = calculate_subtotal(CART_ITEM_DATA)
+    expect(checkout_step_2_page_with_item.subtotal).to_contain_text(expected_subtotal)
 
 
 def test_validate_all_items_total(
     checkout_step_2_page_with_all_items: CheckoutStepTwoPage,
 ) -> None:
-    assert checkout_step_2_page_with_all_items.get_subtotal() == calculate_subtotal(
-        INVENTORY_ITEMS_DATA
+    expected_subtotal: str = calculate_subtotal(INVENTORY_ITEMS_DATA)
+    expect(checkout_step_2_page_with_all_items.subtotal).to_contain_text(
+        expected_subtotal
     )
 
 
 def test_verify_taxes_calculation_for_single_item(
     checkout_step_2_page_with_item: CheckoutStepTwoPage,
 ) -> None:
-    assert checkout_step_2_page_with_item.get_tax() == calculate_taxes(CART_ITEM_DATA)
+    expected_taxes: str = calculate_taxes(CART_ITEM_DATA)
+    expect(checkout_step_2_page_with_item.tax).to_contain_text(expected_taxes)
 
 
 def test_verify_taxes_calculation_for_all_items(
     checkout_step_2_page_with_all_items: CheckoutStepTwoPage,
 ) -> None:
-    assert checkout_step_2_page_with_all_items.get_tax() == calculate_taxes(
-        INVENTORY_ITEMS_DATA
-    )
+    expected_taxes: str = calculate_taxes(INVENTORY_ITEMS_DATA)
+    expect(checkout_step_2_page_with_all_items.tax).to_contain_text(expected_taxes)
