f9a908e2c72e5b22813a02044c0fdbb40bd5b9be
Daniel Molina Ochoa <molina9724@gmail.com>
2026-08-28 09:01:59 -0500

    Refactor Cart component: simplify locator assignments by removing unnecessary constants

--- po/components/cart.py
+++ po/components/cart.py
@@ -7,21 +7,13 @@ from .base_component import BaseComponent
 if TYPE_CHECKING:
     from ..pages.cart_page import CartPage
 
-# Selectors
-CART_BUTTON: str = ".shopping_cart_link"
-CART_COUNTER_BADGE: str = ".shopping_cart_badge"
-
-# Labels
-CART_COUNTER: str = "Cart Counter"
-CART_BUTTON_LABEL: str = "Cart Button"
-
 
 class Cart(BaseComponent):
 
     def __init__(self, page: Page, timeout: int = 10000) -> None:
         super().__init__(page, timeout)
-        self.cart_button: Locator = self.page.locator(CART_BUTTON)
-        self.counter: Locator = self.cart_button.locator(CART_COUNTER_BADGE)
+        self.cart_button: Locator = self.page.locator(".shopping_cart_link")
+        self.counter: Locator = self.cart_button.locator(".shopping_cart_badge")
 
     def get_cart_page(self) -> "CartPage":
         self.cart_button.click()
