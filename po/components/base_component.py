4c685cc544f405602e51dd1a953feea4ff6474f1
Daniel Molina Ochoa <molina9724@gmail.com>
2026-08-28 10:23:48 -0500

    Refactor: clean up imports and remove redundant locator method from BasePage

--- po/components/base_component.py
+++ po/components/base_component.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from playwright.sync_api import Locator, Page
 
 
@@ -9,3 +11,18 @@ class BaseComponent:
     @staticmethod
     def get_parent(locator: Locator) -> Locator:
         return locator.locator("..")
+
+    def locator(self, selector_or_locator: Union[str, Locator]) -> Locator:
+        """
+        Returns a Playwright Locator object for the given selector or locator.
+
+        Args:
+            selector_or_locator (Union[str, Locator]): A CSS selector string or an existing Locator object.
+
+        Returns:
+            Locator: A Playwright Locator object corresponding to the selector or the provided Locator.
+        """
+        if isinstance(selector_or_locator, str):
+            return self.page.locator(selector_or_locator)
+        else:
+            return selector_or_locator
--- po/pages/base_page.py
+++ po/pages/base_page.py
@@ -1,7 +1,3 @@
-from typing import Union
-
-from playwright.sync_api import Locator
-
 from po.components.base_component import BaseComponent
 
 BASE_URL: str = "https://www.saucedemo.com/"
@@ -12,18 +8,4 @@ CHECKOUT_STEP_1_URL: str = BASE_URL + "checkout-step-one.html"
 CHECKOUT_STEP_2_URL: str = BASE_URL + "checkout-step-two.html"
 
 
-class BasePage(BaseComponent):
-    def locator(self, selector_or_locator: Union[str, Locator]) -> Locator:
-        """
-        Returns a Playwright Locator object for the given selector or locator.
-
-        Args:
-            selector_or_locator (Union[str, Locator]): A CSS selector string or an existing Locator object.
-
-        Returns:
-            Locator: A Playwright Locator object corresponding to the selector or the provided Locator.
-        """
-        if isinstance(selector_or_locator, str):
-            return self.page.locator(selector_or_locator)
-        else:
-            return selector_or_locator
+class BasePage(BaseComponent): ...
