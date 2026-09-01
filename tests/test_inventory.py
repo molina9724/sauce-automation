6bdaaf589328a7d74a1344854d1899f801663daa
Daniel Molina Ochoa <molina9724@gmail.com>
2026-09-01 10:09:03 -0500

    Test inventory images around filtering

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,6 +1,9 @@
 # fmt: off
+from typing import List
+from urllib.parse import urljoin
+
 import pytest
-from playwright.sync_api import Locator, expect
+from playwright.sync_api import APIResponse, Locator, expect
 
 from data.global_data import ITEM_INDEX
 from data.inventory_data import (A_TO_Z,
@@ -17,6 +20,21 @@ from po.pages.inventory_page import InventoryPage
 from po.pages.login_page import LoginPage
 
 # fmt: on
+
+
+def assert_images(empty_inventory_page: InventoryPage) -> None:
+    expect(empty_inventory_page.item.image).to_have_count(len(INVENTORY_ITEMS_DATA))
+    images: list[Locator] = empty_inventory_page.item.image.all()
+    for image in images:
+        expect(image).to_be_visible()
+        expect(image).to_have_js_property("complete", True)
+        expect(image).not_to_have_js_property("naturalWidth", 0)
+        source: str | None = image.get_attribute("src")
+        # TODO: Investigate replacing this Python assert with a Playwright attribute assertion
+        assert source
+        image_url: str = urljoin(empty_inventory_page.page.url, source)
+        response: APIResponse = empty_inventory_page.page.request.get(image_url)
+        expect(response).to_be_ok()
 
 
 def test_verify_document_title(empty_inventory_page: InventoryPage) -> None:
@@ -55,36 +73,44 @@ def test_verify_all_product_filter_options(
     expect(empty_inventory_page.all_filter_options).to_have_text(FILTER_OPTIONS)
 
 
-# TODO: This test case should include images and add/remove buttons to consistently test the whole item object
+# TODO: This test case should include add/remove buttons to consistently test the whole item object
 @pytest.mark.parametrize(
     FILTER_ARGS,
     argvalues=FILTER_VALUES,
     ids=[filter_value[0] for filter_value in FILTER_VALUES],
 )
-def test_verify_products_are_sorted_after_selecting_filter(
+def test_verify_products_and_images_after_selecting_filter(
     empty_inventory_page: InventoryPage,
     filter_option: str,
     sort_key: SortKey,
     reverse: bool,
 ) -> None:
+    assert_images(empty_inventory_page)
     # A_TO_Z is the default option, so first we need a change in order to verify
     if filter_option == A_TO_Z:
         empty_inventory_page.set_products_filter(Z_TO_A)
-
     empty_inventory_page.set_products_filter(filter_option)
 
     ordered_items: list[tuple[str, dict[str, str]]] = sorted(
         INVENTORY_ITEMS_DATA.items(), key=sort_key, reverse=reverse
     )
+
     ordered_names: list[str] = [name for name, _ in ordered_items]
+    expect(empty_inventory_page.item.name).to_have_text(ordered_names)
+
     ordered_descriptions: list[str] = [
         details["description"] for _, details in ordered_items
     ]
-    ordered_prices: list[str] = [details["price"] for _, details in ordered_items]
-
-    expect(empty_inventory_page.item.name).to_have_text(ordered_names)
     expect(empty_inventory_page.item.description).to_have_text(ordered_descriptions)
+
+    ordered_prices: list[str] = [details["price"] for _, details in ordered_items]
     expect(empty_inventory_page.item.price).to_have_text(ordered_prices)
+
+    assert_images(empty_inventory_page)
+    images: Locator = empty_inventory_page.item.image
+    expected_image_names: List[str] = [name for name, _ in ordered_items]
+    for index, expected_name in enumerate(expected_image_names):
+        expect(images.nth(index)).to_have_attribute("alt", expected_name)
 
 
 @pytest.mark.anonymous
