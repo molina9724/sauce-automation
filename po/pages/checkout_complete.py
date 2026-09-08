6acacc7caa03fde88aa82a3078570d65b32386e5
Daniel Molina Ochoa <molina9724@gmail.com>
2026-09-08 08:12:40 -0500

    Refactor CheckoutComplete tests to verify visibility and text of the title element

--- po/pages/checkout_complete.py
+++ po/pages/checkout_complete.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Locator, Page
 
 from po.pages.base_page import BasePage
 
@@ -6,3 +6,4 @@ from po.pages.base_page import BasePage
 class CheckoutComplete(BasePage):
     def __init__(self, page: Page, timeout: int = 10000) -> None:
         super().__init__(page, timeout)
+        self.title: Locator = self.page.locator(".title")
--- tests/test_checkout_complete.py
+++ tests/test_checkout_complete.py
@@ -1,8 +1,10 @@
 from playwright.sync_api import expect
 
-from data.routes import CHECKOUT_COMPLETE
 from po.pages.checkout_complete import CheckoutComplete
 
 
-def test(checkout_complete_page_with_item: CheckoutComplete) -> None:
-    expect(checkout_complete_page_with_item.page).to_have_url(CHECKOUT_COMPLETE)
+def test_verify_checkout_complete_message(
+    checkout_complete_page_with_item: CheckoutComplete,
+) -> None:
+    expect(checkout_complete_page_with_item.title).to_be_visible()
+    expect(checkout_complete_page_with_item.title).to_have_text("Checkout: Complete!")
