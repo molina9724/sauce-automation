7ed5f8b94a34bf30d67efa826907673c401eec43
Daniel Molina Ochoa <molina9724@gmail.com>
2026-09-08 07:03:25 -0500

    Add cancel and finish methods to CheckoutStepTwoPage

--- po/pages/checkout_step_2_page.py
+++ po/pages/checkout_step_2_page.py
@@ -1,6 +1,9 @@
 from playwright.sync_api import Locator, Page
 
+from data.routes import CHECKOUT_COMPLETE, INVENTORY
 from po.components.cart_page_item import CartItem
+from po.pages.checkout_complete import CheckoutComplete
+from po.pages.inventory_page import InventoryPage
 
 from ..components.cart import Cart
 from ..components.left_menu import Menu
@@ -16,6 +19,19 @@ class CheckoutStepTwoPage(BasePage):
         self.tax: Locator = self.locator(".summary_tax_label")
         self.total: Locator = self.locator(".summary_total_label")
 
+        self.cancel_button: Locator = self.page.get_by_role("button", name="Cancel")
+        self.finish_button: Locator = self.page.get_by_role("button", name="Finish")
+
         self.item: CartItem = CartItem(page)
         self.menu: Menu = Menu(self.page)
         self.cart: Cart = Cart(self.page)
+
+    def cancel(self) -> InventoryPage:
+        self.cancel_button.click()
+        self.page.wait_for_url(INVENTORY)
+        return InventoryPage(self.page)
+
+    def finish(self) -> CheckoutComplete:
+        self.finish_button.click()
+        self.page.wait_for_url(CHECKOUT_COMPLETE)
+        return CheckoutComplete(self.page)
