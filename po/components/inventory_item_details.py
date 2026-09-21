a53550c0c31305acf84e3ebb53be5f2df9f73b8f
Daniel Molina Ochoa <molina9724@gmail.com>
2026-09-21 07:13:03 -0500

    Refactor InventoryItemDetails to use root locator for improved structure

--- po/components/inventory_item_details.py
+++ po/components/inventory_item_details.py
@@ -6,8 +6,9 @@ from po.components.base_component import BaseComponent
 class InventoryItemDetails(BaseComponent):
     def __init__(self, page: Page, timeout: int = 10000) -> None:
         super().__init__(page, timeout)
-
-        self.name: Locator = self.locator(".inventory_details_name")
-        self.price: Locator = self.locator(".inventory_details_price")
-        self.description: Locator = self.locator(".inventory_details_desc")
-        self.button: Locator = self.locator(".btn_primary")
+        self.root: Locator = self.locator("#inventory_item_container")
+        self.name: Locator = self.root.locator(".inventory_details_name")
+        self.price: Locator = self.root.locator(".inventory_details_price")
+        self.description: Locator = self.root.locator(".inventory_details_desc")
+        self.button: Locator = self.root.get_by_role("button")
+        self.image: Locator = self.root.get_by_role("img")
